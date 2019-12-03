5ebf06a211e08c6b5a777312813141aa83c5a5fd
huabowen <huabowen@finogeeks.com>
2019-12-03 12:01:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, PureComponent, useEffect, useState, useRef, useCallback, } from 'react';
 import './App.css';
 
-function App() {
+const log = console.log.bind(console)
+
+function useCounter(count) {
+  const size = useSize()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>{count}</h1>
+        <h1>{size.height} * {size.width}</h1>
     </div>
-  );
+
+  )
+}
+
+function useCount(defaultCount) {
+  const [count, setCount] = useState(defaultCount)
+  const it = useRef()
+
+  useEffect(() => {
+    log('effect 1')
+    it.current = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000);
+  }, [])
+
+  useEffect(() => {
+    log('effect 2')
+    if (count > 10) {
+      clearInterval(it.current)
+    }
+  })
+
+  return [count, setCount]
+}
+
+function useSize() {
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+  })
+
+  const onResize = useCallback(() => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    })
+  }, [])
+  useEffect(() =>{
+    window.addEventListener('resize', onResize, false)
+    return () => {
+      window.removeEventListener('resize', onResize, false)
+    }
+  }, [])
+  return size
+}
+let id = 0
+
+function App() {
+  log('* render app *', id+=1)
+  const [count, setCount] = useCount(0)
+  const Counter = useCounter(count)
+  const size = useSize()
+
+  
+  return ( 
+    <div>
+      <button
+        type="button"
+        onClick={() => {setCount(count + 1)}}
+      >
+        Click ({count}), {size.width}px * {size.height}px;
+      </button>
+      {Counter}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/component/tryhook.js
@@ -0,0 +1,55 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-18 16:06:44
+ * @LastEditTime: 2019-11-19 14:43:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-app/src/component/tryhook.js
+ */
+
+
+import React, { useState, } from 'react';
+
+
+function TryHook(props) {
+  // 声明一个叫 “count” 的 state 变量。
+  console.log('__ render count __ ')
+  const [name, setName] = useState('time');
+  const [age, setAge] = useState(25);
+  const [count, setCount] = useState(() => {
+    console.log('init count')
+    return props.count || 233;
+  })
+
+    // Similar to componentDidMount and componentDidUpdate:
+  const logData = () => {
+    console.log('name: ', name, ' age: ', age)
+  }
+  const printData = () => {
+    const input = {
+      name,
+      age,
+    }
+    console.log('input data, ', input)
+  }
+
+  const SubButton = ({handleClick}) => {
+    return (
+      <button onClick={handleClick}>subButton</button>
+    )
+  }
+  return (
+    <div>
+      <input onChange={e => setName(e.target.value)} />
+      <span>name: {name}</span>
+      <input onChange={e => setAge(e.target.value)} />
+      <span>age: {age}</span>
+      <button onClick={logData}> submit </button>
+      <SubButton handleClick={printData} />
+      <span>count: {count}</span>
+      <button onClick={() => setCount(count + 1)}>add count</button>
+    </div>
+  );
+}
+
+export default TryHook;
